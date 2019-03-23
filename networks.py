6d3935e1548e62f6c3d82c01b173f617dbcce543
ekazakos <ek17557@bristol.ac.uk>
2019-03-23 21:39:45 +0000

    Update networks.py

--- networks.py
+++ networks.py
@@ -2,7 +2,7 @@
 This file contains definitions for different network architectures.
 """
 from collections import OrderedDict
-from lasagne.layers import InputLayer, Conv2DLayer, MaxPool2DLayer, DenseLayer, dropout, ElemwiseMergeLayer, concat,LocallyConnected2DLayer, reshape, Conv1DLayer, ElemwiseSumLayer
+from lasagne.layers import InputLayer, Conv2DLayer, MaxPool2DLayer, DenseLayer, dropout, ElemwiseMergeLayer, concat,reshape, Conv1DLayer, ElemwiseSumLayer
 import lasagne.nonlinearities
 import lasagne
 import theano.tensor as T
